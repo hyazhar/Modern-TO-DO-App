1ab243dd33ef965a01c047717a2b2d53b2d4aff1
hyazhar <uddinmdazhar21@gmail.com>
2025-11-27 09:50:10 +0530

    Todo Funcationality added successfully with the help of Js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Hamburger Menu Toggle
 const menu = document.querySelector(".menus");
 const hamburger = document.getElementById("hamburger-icon");
+let addtodo=document.querySelector("#addtodo");
 
+addtodo.addEventListener("click",()=>{
+    let todo=document.querySelector("#todolist");
+    let todosection=document.querySelector(".todo-list");
+    let todoitem=document.createElement("li");
+    let delbtn=document.createElement("span");
+    let todoitemname=document.createElement("span");
+    todoitemname.innerText=todo.value;
+    delbtn.innerText=" X ";
+    delbtn.classList.add("delete");
+    todoitem.appendChild(todoitemname);
+     delbtn.addEventListener("click",()=>{
+        todosection.removeChild(todoitem);
+    });
+    todoitem.appendChild(delbtn);
+    todosection.appendChild(todoitem);
+})
 hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
-
 
 // Dark / Light Theme Toggle
 const lightIcon = document.getElementById("light-mode-icon");
